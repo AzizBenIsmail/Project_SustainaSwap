e7502a2157794558646cc0ade8c4a4b9788cb5f3
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2023-09-30 01:34:23 +0100

    Création de la base des données

--- app/Console/Commands/dbcreate.php
+++ app/Console/Commands/dbcreate.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class dbcreate extends Command
 {
@@ -11,22 +12,32 @@ class dbcreate extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'db:create {name?}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
-
+    protected $description = 'Create a new MySQL database based on the database config file or the provide d name';
     /**
+
      * Execute the console command.
+
      *
+
      * @return int
+
      */
+
     public function handle()
     {
-        return Command::SUCCESS;
+        $schemaName = $this->argument('name') ?: config("database.connections.mysql.database");
+        $charset = config("database.connections.mysql.charset",'utf8mb4');
+        $collation = config("database.connections.mysql.collation",'utf8mb4_unicode_ci');
+        config(["database.connections.mysql.database" => null]);
+        $query = "CREATE DATABASE IF NOT EXISTS $schemaName CHARACTER SET $charset COLLATE $collation;";
+        DB::statement($query);
+        config(["database.connections.mysql.database" => $schemaName]);
     }
 }
