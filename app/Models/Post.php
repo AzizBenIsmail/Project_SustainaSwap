2987ef0da524d6986d3a2714e10e8e22b3523776
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2023-10-01 21:56:24 +0100

    crud post , Comment (model creation) and fake data preperation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'content', 'image_url', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->belongsToMany(User::class, 'post_likes', 'post_id', 'user_id');
+    }
 }
--- database/migrations/2023_09_30_002558_create_posts_table.php
+++ database/migrations/2023_09_30_002558_create_posts_table.php
@@ -15,8 +15,27 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->string('image_url')->nullable();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
-        });
+
+            $table->foreign('user_id')->references('id')->on('users');
+                });
+
+
+
+
+                Schema::create('post_likes', function (Blueprint $table) {
+                    $table->id();
+                    $table->unsignedBigInteger('post_id');
+                    $table->unsignedBigInteger('user_id');
+                    $table->timestamps();
+        
+                    $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+                    $table->foreign('user_id')->references('id')->on('users');
+                });
     }
 
     /**
@@ -27,5 +46,6 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('posts');
+        Schema::dropIfExists('post_likes');
     }
 };
