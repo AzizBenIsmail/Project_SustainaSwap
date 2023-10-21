3c61639906d5cbb2de3a96915666f576ee42e96c
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-10-21 15:54:25 +0100

    refact: changing default redirectopn after login based on role - Controllers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // Default redirection
+    //protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -37,4 +39,19 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    //This function does not exist by default
+    //It overwrites the default behavior after redirection which is on => line 31 => redirection to home
+    protected function authenticated(Request $request)
+    {
+        //If the user is an admin or superAdmin
+        if (auth()->user()->isSuperAdmin() || auth()->user()->isAdmin()) {
+            //Redirect to admin layout
+            return redirect('/admin');
+        } else
+            //If role is use
+            //redirect to user layout
+            return redirect('/user');
+    }
+
 }
