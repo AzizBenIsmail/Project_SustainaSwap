6f70b74e6e0d7853908810aa00f7a4a2dca11bbe
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-10-22 00:35:57 +0100

    fea: adding Admin functions in AdminController - Controllers

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
+use App\Models\User;
+
+
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -11,5 +15,55 @@ class AdminController extends Controller
     {
         //dd('here');
         return view('Back_office.dashboard');
+    }
+    public function create()
+    {
+        return view('users.create')
+            ->with('roles',Role::all());
+    }
+    public function getAll()
+    {
+//        dd('here');
+        $users = User::orderBy('created_at','desc')->paginate(3);
+//        dd(count($users));
+        return view('users.index')->with('users',$users);
+    }
+
+    public function grantAdminPrivileges(int $id)
+    {
+        $user= User::find($id);
+        if ($user) {
+            $user->role_id = 2;
+            $user->save();
+        }
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Administrator Privileges Granted Successfully!'
+        ]);
+    }
+    public function revokeAdminPrivileges(int $id)
+    {
+        $user= User::find($id);
+        if ($user) {
+            $user->role_id = 3;
+            $user->save();
+        }
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Administrator Privileges Revoked Successfully!'
+        ]);
+
+    }
+    public function deleteUser(int $id)
+    {
+        $user= User::find($id);
+        if ($user) {
+            $user->delete();
+        }
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User Deleted Successfully!'
+        ]);
+
     }
 }
