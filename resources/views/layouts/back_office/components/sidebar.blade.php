ce662bbf3ef52854ba6f4c1f9b834f86eba40612
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-10-21 17:05:06 +0100

    fix: merge conflict from pulling - Views

--- resources/views/layouts/back_office/components/sidebar.blade.php
+++ resources/views/layouts/back_office/components/sidebar.blade.php
@@ -32,6 +32,21 @@
                         <i class="mdi mdi-note-plus"></i>
                         <span class="hide-menu"> Form Wizard</span></a>
                 </li>
+
+               <li class="sidebar-item">
+                   <a class="sidebar-link waves-effect waves-dark sidebar-link" href="itemsAdmin" aria-expanded="false">
+                       <i class="mdi mdi-view-dashboard"></i>
+                       <span class="hide-menu">Item</span>
+                   </a>
+               </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="categories" aria-expanded="false">
+                        <i class="mdi mdi-chart-bar"></i>
+                        <span class="hide-menu">Category</span>
+                    </a>
+                </li>
+
             </ul>
         </li>
 {{--        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"--}}
