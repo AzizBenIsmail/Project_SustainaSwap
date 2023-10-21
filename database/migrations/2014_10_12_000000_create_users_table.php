b3a354c260662e219153d04108396631f09dc07c
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-10-21 15:59:01 +0100

    feat: adding 'role_id' column to User table - Migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            //$table->string('image')->default('default.png');
             $table->string('email')->unique();
+            $table->unsignedBigInteger('role_id')->default(4);
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
