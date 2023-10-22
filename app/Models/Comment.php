d7e263642e246b7671f2748d7433916c2de9f1c6
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2023-10-22 20:55:54 +0100

    update comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -25,4 +25,35 @@ class Comment extends Model
     {
         return $this->belongsToMany(User::class, 'comment_likes', 'comment_id', 'user_id');
     }
+    
+
+    public static function mostCommentedPost()
+    {
+        return Post::select('posts.id', 'posts.title', 'posts.content')
+            ->join('comments', 'posts.id', '=', 'comments.post_id')
+            ->groupBy('posts.id', 'posts.title', 'posts.content')
+            ->orderByRaw('COUNT(comments.id) DESC')
+            ->first();
+    }
+    
+
+    public static function mostRepeatedWord()
+    {
+        // Get all comments and split them into words
+        $comments = Comment::select('text')->get();
+        $words = [];
+        foreach ($comments as $comment) {
+            $commentWords = str_word_count($comment->text, 1);
+            $words = array_merge($words, $commentWords);
+        }
+
+        // Count word occurrences
+        $wordCounts = array_count_values($words);
+
+        // Find the most repeated word
+        arsort($wordCounts);
+        $mostRepeatedWord = key($wordCounts);
+
+        return $mostRepeatedWord;
+    }
 }
