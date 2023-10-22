1076b24886efadd1fdf197630515c8238c9e9e59
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-10-22 15:50:50 +0100

    feat: adding default profile image link in backoffice@navabar file - Views

--- resources/views/layouts/back_office/components/navbar.blade.php
+++ resources/views/layouts/back_office/components/navbar.blade.php
@@ -7,7 +7,7 @@
             <!-- ============================================================== -->
             <a class="navbar-brand" href="index.blade.php">
                 <!-- Logo icon -->
-                
+
                     <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                     <!-- Dark Logo icon -->
                     <img src="{{ asset('back_office/assets/images/logo-icon.png')}}" alt="homepage" class="light-logo" />
@@ -53,7 +53,7 @@
                 <!-- ============================================================== -->
                 <!-- create new -->
                 <!-- ============================================================== -->
-                
+
                 <!-- ============================================================== -->
                 <!-- Search -->
                 <!-- ============================================================== -->
@@ -76,7 +76,7 @@
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="mdi mdi-bell font-24"></i>
                     </a>
-                    
+
                 </li>
                 <!-- ============================================================== -->
                 <!-- End Comment -->
@@ -93,15 +93,15 @@
                             <li>
                                 <div class="">
                                     <!-- Message -->
-                                   
+
                                     <!-- Message -->
-                                    
+
                                     <!-- Message -->
                                     <a href="javascript:void(0)" class="link border-top">
                                         <div class="d-flex no-block align-items-center p-10">
                                                     <span class="btn btn-danger btn-circle"><i
                                                             class="fa fa-link"></i></span>
-                                           
+
                                         </div>
                                     </a>
                                 </div>
@@ -118,12 +118,12 @@
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="{{ asset('back_office/assets/images/users/1.jpg')}}" alt="user" class="rounded-circle" width="31">
+                        <img src="{{ asset('storage/profile_pictures/' . auth()->user()->image) }}" alt="user" class="rounded-circle" width="31">
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end user-dd animated" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="javascript:void(0)"><i class="ti-user me-1 ms-1"></i>
                             My Profile</a>
-                       
+
                         <a class="dropdown-item" onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();" href="javascript:void(0)">
                             <i class="fa fa-power-off me-1 ms-1"></i>Log out
