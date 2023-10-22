73e71161875fde3fd035673afcbe5bd8639da348
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2023-10-22 22:35:24 +0100

    bandel externe

--- resources/views/Home component/Featured_Products.blade.php
+++ resources/views/Home component/Featured_Products.blade.php
@@ -151,6 +151,21 @@
 
 
         </div>
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6535934ba84dd54dc483f1ac/1hdckeom4';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
