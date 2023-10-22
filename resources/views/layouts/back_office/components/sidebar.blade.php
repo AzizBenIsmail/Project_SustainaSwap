4770de706d1f026e4663185b9eea936ea17477b6
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2023-10-22 17:54:33 +0100

    réglage problème backoffice

--- resources/views/layouts/back_office/components/sidebar.blade.php
+++ resources/views/layouts/back_office/components/sidebar.blade.php
@@ -1,8 +1,6 @@
 <nav class="sidebar-nav">
     <ul id="sidebarnav" class="pt-4">
-        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="index.blade.php"
-                aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">SustainaSwap</span></a>
-        </li>
+            
         {{--        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" --}}
         {{--                                     href="charts.blade.php" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span --}}
         {{--                    class="hide-menu">Charts</span></a></li> --}}
